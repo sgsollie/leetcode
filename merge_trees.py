59c4be3b8449ee7b8ad1b3622522c5053f13a44f
sgsollie <ollieyoung1@hotmail.com>
2020-07-14 11:23:54 +0100

    Adding recursive solution for merge trees

--- merge_trees.py
+++ merge_trees.py
@@ -10,14 +10,14 @@
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         
-        if not t1:
+        if not t1:                                              # Base case - return whichever we have
             return t2
         if not t2:
             return t1
         
-        t1.val += t2.val
+        t1.val += t2.val                                        # add existing values together
         
-        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.left = self.mergeTrees(t1.left, t2.left)             # set left and right vals to whatever recursion returns
         t1.right = self.mergeTrees(t1.right, t2.right)
         
         return t1
